8204f0c8bdbe8ac8a1b1f0c8f38e644252e282d9
balajik27 <balajik272003@gmail.com>
2023-05-13 22:54:58 +0530

    add forpython master

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html',context={'wish':'Good day!'})
+
+def welcome(request):
+    return render(request, 'welcome.html')
